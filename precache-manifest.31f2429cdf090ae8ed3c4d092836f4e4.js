1f4507beb4c6942eef369a998b4c123110d77c7b
xiedong <xieguodong@chinanews.com.cn>
2020-05-28 14:14:54 +0800

    Updates

---
+++ precache-manifest.31f2429cdf090ae8ed3c4d092836f4e4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81909a0b8fb3863c5389c27ae399c9f3",
+    "url": "/web/index.html"
+  },
+  {
+    "revision": "34b9ac771b60a96533e1",
+    "url": "/web/static/css/main.d3aaa3ee.chunk.css"
+  },
+  {
+    "revision": "38037211e8f0bc734e0e",
+    "url": "/web/static/js/2.ccc29bd7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/web/static/js/2.ccc29bd7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "34b9ac771b60a96533e1",
+    "url": "/web/static/js/main.90d653b9.chunk.js"
+  },
+  {
+    "revision": "fe92eb17469ee13180e5",
+    "url": "/web/static/js/runtime-main.fc5f5595.js"
+  }
+]);
